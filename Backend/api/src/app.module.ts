f1c974e8a0f6b302d46a8ee348604ec6082ebbd7
BrayamHC <brayam1957@gmail.com>
2026-06-10 12:13:46 -0600

    refactor(backend): ajusta app module conn middleware

--- Backend/api/src/app.module.ts
+++ Backend/api/src/app.module.ts
@@ -1,19 +1,60 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { ZodSerializerInterceptor } from 'nestjs-zod';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { databaseConfig } from './config/database.config';
 import { DatabaseModule } from './config/database.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { AuthMiddleware } from './middleware/auth.middleware';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [databaseConfig],
+      envFilePath: '.env',
     }),
     DatabaseModule,
+    AuthModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 60,
+      },
+    ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ZodSerializerInterceptor,
+    },
+  ],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .exclude(
+        { path: 'auth/login', method: RequestMethod.POST },
+        { path: 'docs', method: RequestMethod.GET },
+        { path: 'docs-json', method: RequestMethod.GET },
+        { path: 'docs/*splat', method: RequestMethod.ALL },
+      )
+      .forRoutes('*');
+  }
+}
