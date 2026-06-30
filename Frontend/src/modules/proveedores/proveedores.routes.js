37f645f41dcc7d740a85fd90e13f79055f072df0
BrayamHC <brayam1957@gmail.com>
2026-06-30 12:44:21 -0600

    feat(frontend): agrega nueva ruta de proveedor

--- Frontend/src/modules/proveedores/proveedores.routes.js
+++ Frontend/src/modules/proveedores/proveedores.routes.js
@@ -3,19 +3,19 @@ const proveedoresRoutes = [
     path: 'proveedores',
     name: 'ProveedoresGestor',
     component: () => import('./views/ProveedoresGestor.view.vue'),
-    meta: {
-      requiresAuth: true,
-      title: 'Proveedores | FarmaQ IA',
-    },
+    meta: { requiresAuth: true, title: 'Proveedores | FarmaQ IA' },
   },
   {
     path: 'proveedores/crear',
     name: 'CrearProveedor',
     component: () => import('./views/ProveedoresCrear.view.vue'),
-    meta: {
-      requiresAuth: true,
-      title: 'Crear proveedor | FarmaQ IA',
-    },
+    meta: { requiresAuth: true, title: 'Crear proveedor | FarmaQ IA' },
+  },
+  {
+    path: 'proveedores/:uuid',
+    name: 'DetalleProveedor',
+    component: () => import('./views/ProveedoresDetalle.view.vue'),
+    meta: { requiresAuth: true, title: 'Detalle proveedor | FarmaQ IA' },
   },
 ];
 
